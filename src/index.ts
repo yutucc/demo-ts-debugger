65188c06f89e751156ccef0ac54a0d3334283346
wuqinfa <369724377@qq.com>
2022-09-16 17:21:46 +0800

    chore: 尝试 debugger

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+/*
+ * @Author: wuqinfa
+ * @Date: 2022-09-16 17:06:29
+ * @LastEditors: wuqinfa
+ * @Description: 
+ */
+// import sum from '@/utils/sum';
+import sum from './utils/sum';
+
+const first: number = 1;
+const second: number = 3;
+console.log('first :>> ', first);
+console.log('second :>> ', second);
+debugger
+const result = sum(first, second);
+
+console.log('result :>> ', result);
---
+++ src/utils/sum.ts
@@ -0,0 +1,10 @@
+/*
+ * @Author: wuqinfa
+ * @Date: 2022-09-16 17:07:47
+ * @LastEditors: wuqinfa
+ * @Description: 
+ */
+export default (a: number, b: number): number => {
+  debugger
+  return a + b;
+}
